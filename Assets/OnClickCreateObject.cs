2fcd07c2a1e11cb9e26a0c91c387dbf4e2fa8069
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-06-13 19:16:07 +0300

    Added Some features
    Added distance check on board .
    Base tower when added enemy portal . it hast to   some distance  far .

--- Assets/OnClickCreateObject.cs
+++ Assets/OnClickCreateObject.cs
@@ -3,19 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnClickCreateObject : MonoBehaviour {
-	public GameObject prefab;
+	public GameObject BaseTower;
+	public GameObject Enemy;
+
+	int Counter = 0;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-	public void PutObject(Vector2 mousePosition)
+
+
+	// Update is called once per frame
+	void Update () {
+		//base flag 
+
+		if (Counter == 0 && Input.GetMouseButtonDown (0)) {
+			PutObject (Input.mousePosition,BaseTower);	
+			//Debug.Log("XXXXXXXXXXXXXX");
+			//PrintMousePosition ();
+			++Counter;
+		}else if (Counter == 1 && Input.GetMouseButtonDown (0)) {
+
+			RaycastHit hit = RayFromCamera(Input.mousePosition, 1000.0f);
+			var TowerPosition = BaseTower.transform.position;
+
+
+			var Distance = Vector3.Distance (BaseTower.transform.position, hit.point);
+			//Distance check part
+			Debug.Log ("------------->  " + Distance);
+			if (Distance > 1.43) {
+				Debug.Log ("------------->  Buraya Koyulamaz " );
+
+			} else {
+			
+				PutObject (Input.mousePosition,Enemy);
+				++Counter;
+
+			}
+			//var distance = Vector3.Distance(BaseTower.transform.position, MousePos.transform.position);
+
+
+			//Debug.Log("XXXXXXXXXXXXXX");
+			//PrintMousePosition ();
+		}
+
+	}
+
+	public void PutObject(Vector2 mousePosition,GameObject ob)
 	{
 		RaycastHit hit = RayFromCamera(mousePosition, 1000.0f);
-
-		if(hit.collider.tag=="Table")
-			GameObject.Instantiate(prefab, hit.point, Quaternion.identity);
+	if (hit.collider.tag == "Table") {
+			Vector3 point = hit.point;
+			point.y +=(float)0.05;
+			GameObject.Instantiate (ob, point, Quaternion.identity);
+		}
 	}
 
 	public RaycastHit RayFromCamera(Vector3 mousePosition, float rayLength)
@@ -24,18 +67,6 @@ public class OnClickCreateObject : MonoBehaviour {
 		Ray ray = Camera.main.ScreenPointToRay(mousePosition);
 		Physics.Raycast(ray, out hit, rayLength);
 		return hit;
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		if (Input.GetMouseButtonDown (0)) {
-
-			PutObject (Input.mousePosition);	
-			Debug.Log("XXXXXXXXXXXXXX");
-			PrintMousePosition ();
-		}
-
 	}
 
 	void PrintMousePosition(){
