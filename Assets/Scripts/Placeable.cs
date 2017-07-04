58f9ae6e0c0b17c769ab4e68c0d5c430cfef073e
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-07-04 13:57:20 +0300

    updated

---
+++ Assets/Scripts/Placeable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour {
+	public GameObject placerModel;
+	public GameObject placedModel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void HideAll(){
+		placerModel.SetActive(false);
+		placedModel.SetActive(false);
+	}
+
+	public bool isDistanceCheck(List<Vector3> positionList){
+
+		if(positionList.Count == 0)
+        	positionList.Add( Cursor3D.instance.PositionOnTable);
+
+		for(int i = 1 ; i < (positionList.Count); ++i){
+			if(!DistanceCheckWith2Objects(positionList[i],Cursor3D.instance.PositionOnTable,0.3)){
+				return false;
+			}
+
+			float distance = Vector3.Distance(positionList[i],Cursor3D.instance.PositionOnTable);
+			print("Distance to other: " + distance);
+
+		}
+		return true;
+	}
+
+	//About Distance
+	public bool DistanceCheckWith2Objects(Vector3 ObPos,Vector3 ObPos2,double distance){
+		if(FindDistance(ObPos,ObPos2) > distance)
+			return true;		
+		return false;
+	}
+
+	public double FindDistance(Vector3 a, Vector3 b)
+     {
+         return Vector3.Distance(a,b);
+     }
+
+	//About Distance*/
+
+}
