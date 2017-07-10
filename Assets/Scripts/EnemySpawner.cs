1d39e13cb668e0b40add1b10de9cd095dad72115
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-07-10 09:31:11 +0300

    About Enemy Tower
    Add attribute enemy tower.its swapn enemy in one direction

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public bool stop;
+	public GameObject Enemy;
+
+	public float spawnRate= 1.0f;
+	float timeLeftToSpawn;
+
+	
+	// Use this for initialization
+	void Start () {
+		timeLeftToSpawn= spawnRate;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeftToSpawn-= Time.deltaTime;
+		if(timeLeftToSpawn < 0 && !stop) {
+			Spawn();
+			timeLeftToSpawn= spawnRate;
+		}
+	}
+
+	void Spawn(){
+		GameObject enemyObject=  Instantiate(Enemy) as GameObject;
+		enemyObject.transform.position= transform.position;
+
+	}
+}
