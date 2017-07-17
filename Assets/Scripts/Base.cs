ee2fbc060c5b8332a925b95c004bc875057a8ef9
Celaleddin Subaşı <celoron@gmail.com>
2017-07-17 18:38:54 +0300

    enemy building changes

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : Building {
+    public override void DestroyBuilding() {
+        base.DestroyBuilding();
+
+        Debug.Log("You lose nuub");
+    }
+}
---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+    public int health= 3;
+
+    public void TakeDamage(int dmg) {
+        health -= dmg;
+
+        if(health <= 0) {
+            DestroyBuilding();
+        }
+    }
+
+    public virtual void DestroyBuilding() {
+        Destroy(transform.parent.gameObject);
+    }
+}
