4c866f487ea6eced3dd93e4a737000226c074c8a
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-07-17 17:17:59 +0300

    Find Enemy Spawn something
    not working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public ClosestBuilding close;
+
+
 	public float rayDistance = 3.0f;
 
 	public LayerMask layer;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Vector3 control = FindEnemy();
-		transform.Translate(1f*(Time.deltaTime)/2,0f,0f);
-	}
+        Vector3 target_position = close.closest_item();
+        Debug.Log(target_position.x + "  " + target_position.y + " " + target_position.z + " *\n");
+        transform.Translate(1f * (Time.deltaTime) / 2, 0f, 1f * (Time.deltaTime) / 2);
+
+    }
 
 
-/*	Vector3 FindEnemy(){
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if(Physics.Raycast(this.transform.position,hit,rayDistance,layer.value)){
-			Debug.DrawLine(ray.origin,hit.point,Color.green);
-			if(hit.collider!=null &&hit.transform.tag.Equals("Friend")){
-				Debug.Log("Hit the friend buildingss");	
-				return hit.point;
-			}
-		}
-		return new Vector3(0,0,0);
-	}*/
+    /*	Vector3 FindEnemy(){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(this.transform.position,hit,rayDistance,layer.value)){
+                Debug.DrawLine(ray.origin,hit.point,Color.green);
+                if(hit.collider!=null &&hit.transform.tag.Equals("Friend")){
+                    Debug.Log("Hit the friend buildingss");	
+                    return hit.point;
+                }
+            }
+            return new Vector3(0,0,0);
+        }*/
 }
