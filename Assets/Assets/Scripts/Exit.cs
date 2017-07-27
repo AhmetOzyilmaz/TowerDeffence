a4fde1186664cd529934023a3d6dcf0dec2e255e
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-07-27 13:05:53 +0300

    Added menü
    its basic menu when on click start game text loading game scene

---
+++ Assets/Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems; // Required when using Event data.
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Application.Quit();
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+
+    }
+}
---
+++ Assets/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems; // Required when using Event data.
+using UnityEngine.UI;
+
+public class LoadGame : MonoBehaviour, IPointerDownHandler
+{
+     public void OnPointerDown(PointerEventData eventData)
+    {
+        Application.LoadLevel("GameBoard");
+    }
+}
